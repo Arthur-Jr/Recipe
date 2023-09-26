369c0c42746fc23a9e4d35598d429034b0d98140
Arthur Junior <arthurjr1906@gmail.com>
2023-09-26 02:34:23 -0300

    feat: Add meal categories

---
+++ src/constants/mealsCategories.js
@@ -0,0 +1,11 @@
+export const categories = {
+  'café da manhã': 'Breakfast',
+  'sobremesa': 'Dessert',
+  'diversas': 'Miscellaneous',
+  'massa': 'Pasta',
+  'frutos do mar': 'Seafood',
+  'side': 'Side',
+  'entrada': 'Starter',
+  'vegano': 'Vegan',
+  'vegetariano': 'Vegetarian'
+}
