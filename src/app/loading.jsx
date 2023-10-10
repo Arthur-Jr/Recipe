e55d3c053dd4cb64ed0190635f35cba76ef2828c
Arthur Junior <arthurjr1906@gmail.com>
2023-10-10 01:33:06 -0300

    feat: add page loading

---
+++ src/app/loading.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Loading() {
+  return (
+    <div className="flex justify-center items-center h-28 w-full">
+      <span className="font-bold text-lg text-center text-primary-color">Loading...</span>
+    </div>
+  )
+}
