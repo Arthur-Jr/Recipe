f58c2a4115fb00ab629fa79e110a5b1ec6064880
Arthur Junior <arthurjr1906@gmail.com>
2023-10-11 18:21:14 -0300

    feat: Add texts tha can be translated

---
+++ src/constants/texts.js
@@ -0,0 +1,16 @@
+export const texts = {
+  PT: {
+    name: 'Nome',
+    ingredient: 'Ingrediente',
+    searchBy: 'Buscar por',
+    texinputPlaceHolder: 'O que vamos comer hoje?',
+    searchBtn: 'Pesquisar',
+  },
+  EN: {
+    name: 'Name',
+    ingredient: 'Ingredient',
+    searchBy: 'Search by',
+    texinputPlaceHolder: 'What we eating today?',
+    searchBtn: 'Search',
+  }
+}
